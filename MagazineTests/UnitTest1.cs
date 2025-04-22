9d44f57ce1abb5f8ab067c2aff68b7c6ffe140ea
Aleksandra <aleksavs2004@mail.ru>
2025-04-22 19:03:34 +0300

    Создала проект модульного тестирования и добавила ссылки на проекты

---
+++ MagazineTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace MagazineTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
